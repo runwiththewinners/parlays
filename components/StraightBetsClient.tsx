212b6894340d15ed76dd7696d2b19d9380b26873
Ian Singh <iansingh@ians-mbp-2.mynetworksettings.com>
2026-02-15 09:09:28 -0500

    Add experience route + notifications

--- components/StraightBetsClient.tsx
+++ components/StraightBetsClient.tsx
@@ -1243,6 +1243,8 @@ export default function StraightBetsClient({
               marginBottom: 4,
             }}
           >
+            <span style={{ color: "#f5f5f5" }}>Par</span>
+            <br />
             <span
               style={{
                 background:
@@ -1251,7 +1253,7 @@ export default function StraightBetsClient({
                 WebkitTextFillColor: "transparent",
               }}
             >
-              Parlays
+              lays
             </span>
           </h1>
 
